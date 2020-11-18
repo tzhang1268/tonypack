a3f188fc8efd1df8a63d92fcda7e1073090deba8
tzhang1268 <t.c.zhang@hotmail.com>
2020-11-18 01:13:26 -0800

    2

--- R/days_to_ny.R
+++ R/days_to_ny.R
@@ -1,14 +1,27 @@
+#' Days to New Years
+#'
+#' @param inputdate Date
+#'
+#' @return
+#' Outputs "Days to New Years for the input date(s):" + a vector of days to New Years
+#'
+#' @export
+#'
+#' @examples
+#' days_to_ny(ymd("2020-10-22"))
+#' days_to_ny(as_date(lubridate::ymd("2020-10-22"):ymd("2020-10-29")))
+#'
 days_to_ny <- function(inputdate){
 
   # Adds a warning if non-Date inputs are detected, and tells the user what format (other than Date) they are using in the input. I used warning() instead of stop() since each calculation is independent of each other. So using warning() tells the user about the issue while still outputting the remaining Date inputs if they are in the valid format.
-  if(!is.Date(inputdate)) {
+  if(!lubridate::is.Date(inputdate)) {
     warning("Warning, your input(s) contain data not in Date format. Your input contains the format: ", class(pickdate))}
 
   # Adding a verbose message to update the user that the tool is running as intended.
   print("Calculating...")
 
   # Determines the start of the following year
-  new_yr <- (ceiling_date(inputdate,"year"))
+  new_yr <- (lubridate::ceiling_date(inputdate,"year"))
 
   # Subtract the new year from current date
   daysleft <- (new_yr - inputdate)
